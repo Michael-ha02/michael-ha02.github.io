bf8bfaf207dea48edf8037ade6b1de4fd4d5275e
Michael Ha <michaelhabao@gmail.com>
2023-04-12 15:27:31 -0400

    update route

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/list100", function (req, res) {
+  res.sendFile(__dirname + "/list100.html");
+});
+app.get("/blog", function (req, res) {
+  res.sendFile(__dirname + "/book.html");
+});
+app.get("/books", function (req, res) {
+  res.sendFile(__dirname + "/books.html");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
